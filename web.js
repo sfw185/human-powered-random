191358076d47c4d9a268e61366ec7911767de382
Farid Wardan <farid@wardan.com>
2014-03-31 15:06:52 +1100

    First semi-working commit

--- web.js
+++ web.js
@@ -1,8 +1,49 @@
-var express = require("express");
+var express = require('express');
+var logfmt = require('logfmt');
+var https = require('https');
+var jade = require('jade');
+
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.use(logfmt.requestLogger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World! See <a href="/generate">/generate</a>').end();
+});
+
+app.get('/generate', function(request, response) {
+
+  var options = {
+    path: '/v1/media/popular?client_id=5d8fd13a49b949279f0ee9eb5b11f65b',
+    host: 'api.instagram.com',
+    headers: {'user-agent': 'Human-powered Randomness (https://github.com/FaridW/human-rng)'}
+  };
+
+  https.get(options, function(res) {
+    var body = '';
+
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function(){
+      var api_response = JSON.parse(body);
+      var data = api_response.data;
+
+      response.send(api_response);
+      response.end();
+
+      for (var i = 0; i < data.length; i++)
+      {
+        var media = data[i];
+        var web_url = media.link;
+        var image_url = media.images.standard_resolution.url;
+        var caption = media.caption.text;
+
+        console.log(caption + ' ' + web_url + ' ' + image_url);
+      }
+    });
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
